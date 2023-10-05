0f9a90eeb8dd7822dbdf03bc50727d733793cbe9
lowkeyboiz1 <khangluong20021@gmail.com>
2023-10-05 09:39:37 +0700

    bug ts

--- src/modules/employee-management/employee-tab/index.tsx
+++ src/modules/employee-management/employee-tab/index.tsx
@@ -4,6 +4,8 @@ import { Button, Input } from '@nextui-org/react'
 import { SearchIcon } from '@/components/icon'
 import { Add, Filter } from 'iconsax-react'
 import TableComponent from '@/components/table/table'
+import Pagi from '@/components/pagination'
+import { useState } from 'react'
 
 function Customer() {
   const columns = [
@@ -88,6 +90,15 @@ function Customer() {
       deparment: 'Thợ sửa máy lạnh',
     },
   ]
+
+  //pagination
+  const [page, setPage] = useState<number>(1)
+  const rowsPerPage = 3
+
+  const [select, setSelect] = useState<boolean>(false)
+
+  const [listSelected, setListSelected] = useState([])
+
   return (
     <div className="">
       <div className={styles.spacing}>
@@ -134,8 +145,20 @@ function Customer() {
         <div className="mt-8">
           <TableComponent
             columns={columns}
+            page={page}
+            setPage={setPage}
+            rowsPerPage={rowsPerPage}
             initialData={initialData}
-            rowsPerPage={8}
+            multiSelectTable={select ? 'multiple' : 'single'}
+            handleSelected={setListSelected}
+          />
+        </div>
+        <div className="absolute bottom-5 w-full">
+          <Pagi
+            totalItem={8}
+            page={page}
+            onChange={page => setPage(page)}
+            totalPage={Math.ceil(initialData.length / rowsPerPage)}
           />
         </div>
       </div>
